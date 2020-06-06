e919344eec33276c679322b7dad05aa7dbff071a
Jake Morris <morrisj32@cardiff.ac.uk>
2020-06-06 13:48:25 +0100

    Reset base application Base files are appended with an '_base'

---
+++ components/Alert.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="alert">
+    Alert name
+  </div>
+</template>
+
+<style scoped>
+.alert {
+  width: 100%;
+  background: red;
+  padding: 0.5rem;
+  font-weight: 600;
+  color: white;
+  text-align: center;
+}
+</style>
