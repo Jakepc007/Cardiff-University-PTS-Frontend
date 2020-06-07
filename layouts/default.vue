98895798f308810eead3400106de2f7744ed195c
Jake Morris <morrisj32@cardiff.ac.uk>
2020-06-07 11:31:58 +0100

    Imported google roboto font in main layout file

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,14 @@
 <template>
   <v-content>
-    Default layout:
     <nuxt />
   </v-content>
 </template>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
 * {
+  font-family: 'Roboto', sans-serif;
   padding: 0;
   margin: 0;
 }
