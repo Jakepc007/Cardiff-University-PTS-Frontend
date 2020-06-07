c69be50f3b716c78a5722a95cb41b28cf545b062
Jake Morris <morrisj32@cardiff.ac.uk>
2020-06-07 11:31:43 +0100

    Default alert defined for testing, need to remove later

--- components/Alert.vue
+++ components/Alert.vue
@@ -1,14 +1,36 @@
 <template>
-  <div>Alert Component {{ alert }}</div>
+  <div class="alert" :class="alert.level">
+    {{ alert.alert }}
+  </div>
 </template>
 
 <script>
 export default {
   props: {
     alert: {
-      type: String,
-      default: 'IDK'
+      type: Object,
+      default() {
+        return {
+          id: 2,
+          alert: 'Danger text',
+          level: 'danger'
+        }
+      }
     }
   }
 }
 </script>
+
+<style scoped>
+.alert {
+  width: 100%;
+  padding: 0.25rem;
+  font-weight: 600;
+  text-align: center;
+}
+
+.info {
+  background: rgb(151, 151, 0);
+  color: white;
+}
+</style>
