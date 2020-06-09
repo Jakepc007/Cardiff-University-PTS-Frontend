d6fa7e1a762c17c533d74e7afb636e3b2faf0753
Jake Morris <morrisj32@cardiff.ac.uk>
2020-06-09 05:06:36 +0100

    Decorated record card some more, aligned it to center It will now take up the whole page, instead of being split

--- components/RecordCard.vue
+++ components/RecordCard.vue
@@ -1,22 +1,28 @@
 <template>
-  <router-link :to="{ params: { id: r.id } }">
-    <v-card
-      class="record-card"
-      :raised="hover"
-      ripple
-      outlined
-      width="full"
-      @mouseover="hover = true"
-      @mouseleave="hover = false"
-    >
-      <v-list-item three-line>
-        <v-list-item-content>
-          <div>{{ r.title }}</div>
-          <v-list-item-subtitle>{{ r.desc }}</v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-    </v-card>
-  </router-link>
+  <v-hover>
+    <template v-slot="{ hover }">
+      <v-lazy
+        :options="{
+          threshold: 1
+        }"
+        transition="scroll-x-transition"
+      >
+        <v-card class="record-card" ripple outlined :elevation="hover ? 2 : 0">
+          <router-link :to="{ path: 'records/' + r.id }">
+            <v-list-item three-line>
+              <v-list-item-content>
+                <div>
+                  <span class="red--text">{{ r.id }} | </span>
+                  <span>{{ r.title }}</span>
+                </div>
+                <v-list-item-subtitle>{{ r.status }}</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </router-link>
+        </v-card>
+      </v-lazy>
+    </template>
+  </v-hover>
 </template>
 
 <script>
@@ -28,18 +34,14 @@ export default {
         return {}
       }
     }
-  },
-  data() {
-    return {
-      hover: false
-    }
   }
 }
 </script>
 
 <style scoped>
 .record-card {
-  border-radius: 100px;
+  margin-left: auto;
+  margin-right: auto;
   background: rgb(255, 238, 238);
 }
 
