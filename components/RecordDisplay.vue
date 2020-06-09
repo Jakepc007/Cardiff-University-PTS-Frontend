f12b9133740b3d58d55b4cd1b650f33b12f16aef
Jake Morris <morrisj32@cardiff.ac.uk>
2020-06-09 05:07:16 +0100

    Added a link to return to the records page from within a record display

--- components/RecordDisplay.vue
+++ components/RecordDisplay.vue
@@ -1,17 +1,22 @@
 <template>
-  <v-lazy
-    :options="{
-      threshold: 1
-    }"
-    transition="fade-transition"
-  >
-    <div class="record-display">
-      <p>{{ record.title }}</p>
-      <p class="grey--text text--darken-2">
-        {{ record.desc }}
-      </p>
-    </div>
-  </v-lazy>
+  <div>
+    <nuxt-link to="/records"
+      ><v-icon large>mdi-checkbox-marked-circle</v-icon></nuxt-link
+    >
+    <v-lazy
+      :options="{
+        threshold: 1
+      }"
+      transition="fade-transition"
+    >
+      <div class="record-display">
+        <p>{{ record.title }}</p>
+        <p class="grey--text text--darken-2">
+          {{ record.desc }}
+        </p>
+      </div>
+    </v-lazy>
+  </div>
 </template>
 
 <script>
@@ -26,6 +31,9 @@ export default {
 </script>
 
 <style>
+a {
+  text-decoration: none;
+}
 .record-display {
   padding: 2rem;
 }
