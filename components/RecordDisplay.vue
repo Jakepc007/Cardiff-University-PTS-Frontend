822fc76fb2a641f7251687e0d7dcc883ba94cb60
Jake Morris <morrisj32@cardiff.ac.uk>
2020-06-09 04:51:24 +0100

    Added seperate page to display records

---
+++ components/RecordDisplay.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-lazy
+    :options="{
+      threshold: 1
+    }"
+    transition="fade-transition"
+  >
+    <div class="record-display">
+      <p>{{ record.title }}</p>
+      <p class="grey--text text--darken-2">
+        {{ record.desc }}
+      </p>
+    </div>
+  </v-lazy>
+</template>
+
+<script>
+export default {
+  props: {
+    record: {
+      default() {},
+      type: Object
+    }
+  }
+}
+</script>
+
+<style>
+.record-display {
+  padding: 2rem;
+}
+</style>
