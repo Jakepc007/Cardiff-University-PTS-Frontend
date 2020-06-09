7466a9fd737424366b98c54914b7b284ef3547e6
Jake Morris <morrisj32@cardiff.ac.uk>
2020-06-09 05:11:36 +0100

    Added class 'app' to nuxt app I'm experimenting with how to best implement styling

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <Alert :alert="alert" />
-    <nuxt />
+    <nuxt class="app" />
   </v-app>
 </template>
 
@@ -16,16 +16,16 @@ export default {
   // The fetch here, allows dispatching of VueX actions,
   // which then retrieve the most recent alert, and can
   // then plug that into the layout
-  async fetch({ store, error }) {
-    try {
-      await store.dispatch('alerts/fetchAlert')
-    } catch (e) {
-      error({
-        statusCode: 503,
-        message: 'Unable to fetch events at this time'
-      })
-    }
-  },
+  // async fetch({ store, error }) {
+  //   try {
+  //     await store.dispatch('alerts/fetchAlert')
+  //   } catch (e) {
+  //     error({
+  //       statusCode: 503,
+  //       message: 'Unable to fetch events at this time'
+  //     })
+  //   }
+  // },
   computed: {
     ...mapState({
       alert: (state) => state.alerts.alert
@@ -34,7 +34,7 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 /* This doesn't seem to do anything, but I will keep it for now... */
 @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
 
@@ -42,5 +42,17 @@ export default {
   font-family: 'Roboto', sans-serif;
   padding: 0;
   margin: 0;
+}
+
+.app {
+  padding-left: 1rem;
+  padding-right: 1rem;
+  margin-left: auto;
+  margin-right: auto;
+  width: 100%;
+
+  @media only screen and (min-width: 900px) {
+    width: 80%;
+  }
 }
 </style>
