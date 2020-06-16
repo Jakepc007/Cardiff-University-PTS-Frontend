bd10adf91a14741f97daeb03bbcf133eac499ce2
Jake Morris <morrisj32@cardiff.ac.uk>
2020-06-16 21:32:31 +0100

    Added form details inside of state

--- store/form.js
+++ store/form.js
@@ -1,13 +1,44 @@
 export const state = () => ({
-  form: {},
-  page: 1
+  form: {
+    title: {
+      label: 'title',
+      value: '',
+      page: 1
+    },
+    status: {
+      label: 'status',
+      hint: 'The applications current status',
+      value: '',
+      page: 1,
+      // TODO fetch these from state (less error prone)
+      options: ['Just a thought', 'Ready for submission']
+    },
+    description: {
+      label: 'description',
+      help: 'Title of the application',
+      value: '',
+      page: 1
+    },
+
+    pi: {
+      label: 'principal investigator',
+      value: '',
+      page: 2
+    }
+  },
+  page: 1,
+  inProgress: false
 })
 
 export const mutations = {
   CHANGE_PAGE(state, pageMod) {
-    // Takes in a positive or negative integer
-    // which increments or deincrements the page
     state.page += pageMod
+  },
+  TOGGLE_IN_PROCESS(state) {
+    state.inProgress = !state.inProgress
+  },
+  RESET_PAGE(state) {
+    state.page = 0
   }
 }
 
@@ -18,6 +49,7 @@ export const actions = {
   prevPage({ commit }) {
     commit('CHANGE_PAGE', -1)
   }
+  // submit({ commit }) { }
 }
 
 export const getters = {}
